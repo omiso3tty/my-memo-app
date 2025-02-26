05f8946597f0e8be35d1643965c24e902fdd6b68
omiso <omiso3tty@gmail.com>
2025-02-26 11:38:51 +0900

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: false,
+  semi: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+}
+
+export default config
